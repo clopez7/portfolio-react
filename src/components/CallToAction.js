db984913f92d4979f327ed3d479eef3d605cd13c
sensei <carlospunk41@gmail.com>
2021-11-06 18:56:22 +0100

    Added basic components

---
+++ src/components/CallToAction.js
@@ -0,0 +1,10 @@
+function CallToAction() {
+    return (
+        <div className="text call-to-action">
+            <h1>Carlos López</h1>
+            <p>Front End Developer</p>
+        </div>
+    );
+  }
+   
+  export default CallToAction;
